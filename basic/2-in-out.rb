5c3ea617672c68d4a7a44e70830b3a1b31af2e50
guimaraesnatalia <nataliasouzaguimaraes@gmail.com>
2022-11-08 15:43:40 -0300

    Add print with plus

--- basic/2-in-out.rb
+++ basic/2-in-out.rb
@@ -5,9 +5,14 @@ puts "HELLO WORLD" #Printa com quebra de linha no final (tipo o println)
 
 variavel = 1234
 puts "Imprimindo variável #{variavel}"
+puts "Imprimindo variável "+ variavel.to_s + " de outra forma"
 
 #IN
 #Lendo string
+print "Digite uma frase: "
 frase = gets.chomp
+puts frase
 #Lendo numero inteiro
+print "Digite um numero inteiro: "
 numero_inteiro = gets.chomp.to_i
+puts numero_inteiro
