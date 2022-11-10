1398869e7d695db6e63eda8741af9c9c47fa74db
guimaraesnatalia <nataliasouzaguimaraes@gmail.com>
2022-11-10 16:14:36 -0300

    Add math

---
+++ intro/9-math.rb
@@ -0,0 +1,11 @@
+# ruby 9-math.rb
+
+#Numeros randomicos
+aleatorio_1 = rand
+puts "Primeiro numero aleatorio: #{aleatorio_1}"
+#Numero aleatorio entre 1 e 30
+aleatorio_2 = rand(30)
+puts "Segundo numero aleatorio: #{aleatorio_2}"
+#Numero aleatorio entre 1 e 30
+aleatorio_3 = (rand * 30).to_i
+puts "Segundo numero aleatorio: #{aleatorio_3}"
