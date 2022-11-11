b5148f28669dca0c32016bdb0c5eb31d97bee919
guimaraesnatalia <nataliasouzaguimaraes@gmail.com>
2022-11-11 10:14:33 -0300

    refactor: i/o using functions

--- intro/2-in-out.rb
+++ intro/2-in-out.rb
@@ -1,23 +1,40 @@
 # ruby 2-in-out.rb
 #OUT
-print "HELLO WORLD" #Não quebra linha no fim da execução
-puts "HELLO WORLD" #Printa com quebra de linha no final (tipo o println)
+def output_simples
+    print "HELLO WORLD" #Não quebra linha no fim da execução
+    puts "HELLO WORLD" #Printa com quebra de linha no final (tipo o println)
+end
 
-variavel = 1234
-puts "Imprimindo variável #{variavel}"
-puts "Imprimindo variável "+ variavel.to_s + " de outra forma"
+def imprime_variavel
+    variavel = 1234
+    puts "Imprimindo variável #{variavel}"
+    puts "Imprimindo variável "+ variavel.to_s + " de outra forma"
+end
 
 #IN
 #Lendo string
-print "Digite uma frase: "
-frase_com_enter = gets
-puts frase_com_enter + "(fim da frase)"
+def lendo_string_gets
+    print "Digite uma frase: "
+    frase_com_enter = gets
+    puts frase_com_enter + "(fim da frase)"
+end
 
-print "Digite uma frase: "
-frase_sem_enter = gets.chomp #o chomps tira a quebra de linha ao final da entrada
-puts frase_sem_enter + "(fim da frase)"
+def le_string_chomp
+    print "Digite uma frase: "
+    frase_sem_enter = gets.chomp #o chomps tira a quebra de linha ao final da entrada
+    puts frase_sem_enter + "(fim da frase)"
+end
 
 #Lendo numero inteiro
-print "Digite um numero inteiro: "
-numero_inteiro = gets.chomp.to_i
-puts numero_inteiro
+def le_numero_inteiro
+    print "Digite um numero inteiro: "
+    numero_inteiro = gets.chomp.to_i
+    puts numero_inteiro
+end
+
+#Inicio das chamadas de funcoes
+output_simples
+imprime_variavel
+lendo_string_gets
+le_string_chomp
+le_numero_inteiro
