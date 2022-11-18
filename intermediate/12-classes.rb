c8fdea89be6256895aea948135498891d90a8869
guimaraesnatalia <nataliasouzaguimaraes@gmail.com>
2022-11-17 22:36:16 -0300

    Update classes-private

--- intermediate/12-classes.rb
+++ intermediate/12-classes.rb
@@ -2,6 +2,16 @@
 
 class BankAccount
     attr_accessor :name, :cpf, :balance
+
+    def present_client_data
+        puts "Client #{name}:"
+        puts "CPF: #{cpf}"
+        puts "Balance: $#{balance}"
+    end
+
+    def sum_two_dollars_balance
+        @balance = balance + 2.0
+    end
 end
 
 client_natalia = BankAccount.new
@@ -9,6 +19,6 @@ client_natalia.name = "Natália Guimarães"
 client_natalia.cpf = "123.456.789-11"
 client_natalia.balance = 30000.0
 
-puts "Client #{client_natalia.name}:"
-puts "CPF: #{client_natalia.cpf}"
-puts "Balance: $#{client_natalia.balance}"
+client_natalia.present_client_data
+client_natalia.sum_two_dollars_balance
+client_natalia.present_client_data
